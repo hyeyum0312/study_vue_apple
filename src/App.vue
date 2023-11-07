d2d4dbd2cb0e82021a67c5d5cd500b1dcb2c5b96
조혜연 <hy_312@kiwisnap.net>
2023-11-07 23:38:33 +0900

    feat: vue 게시물발행

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,153 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+        <ul class="header-button-left">
+            <li>Cancel</li>
+        </ul>
+        <ul class="header-button-right">
+            <li v-if="step === 1" @click="step++">Next</li>
+            <li v-if="step === 2" @click="publish">발행</li>
+        </ul>
+        <img src="./assets/logo.png" class="logo" />
+    </div>
+
+    <Container :listData="list" @addData="addData" :step="step" :fileInfo="fileInfo" />
+
+    <button @click="more">더보기</button>
+    <div class="footer">
+        <ul class="footer-button-plus">
+            <input @change="upload" multiple type="file" id="file" class="inputfile" />
+            <label for="file" class="input-plus">+</label>
+        </ul>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import Container from "./components/Container.vue";
+import listData from "./listData";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const list = ref([...listData]);
+const moreCount = ref(0);
+const step = ref(0);
+const fileInfo = ref("");
+const content = ref("");
+
+function more() {
+    const url = `https://codingapple1.github.io/vue/more${moreCount.value}.json`;
+    axios.get(url).then(function (res) {
+        const response = res.data;
+        list.value.push(response);
+        moreCount.value++;
+    });
+}
+
+function upload(e) {
+    let file = e.target.files;
+    console.log("file", file);
+    step.value++;
+
+    fileInfo.value = URL.createObjectURL(file[0]);
+}
+
+function publish() {
+    step.value = 0;
+    let addItem = {
+        name: "나야나222",
+        userImage: fileInfo.value,
+        postImage: fileInfo.value,
+        likes: 1000,
+        date: "May 15",
+        liked: false,
+        content: content.value,
+        filter: "perpetua",
+    };
+    list.value.unshift(addItem);
+}
+
+function addData(data) {
+    console.log("content", content);
+    content.value = data.value;
 }
 </script>
 
 <style>
+@import "style.css";
+
+body {
+    margin: 0;
+}
+ul {
+    padding: 5px;
+    list-style-type: none;
+}
+.logo {
+    width: 22px;
+    margin: auto;
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 13px;
+}
+.header {
+    width: 100%;
+    height: 40px;
+    background-color: white;
+    padding-bottom: 8px;
+    position: sticky;
+    top: 0;
+}
+.header-button-left {
+    color: skyblue;
+    float: left;
+    width: 50px;
+    padding-left: 20px;
+    cursor: pointer;
+    margin-top: 10px;
+}
+.header-button-right {
+    color: skyblue;
+    float: right;
+    width: 50px;
+    cursor: pointer;
+    margin-top: 10px;
+}
+.footer {
+    width: 100%;
+    position: sticky;
+    bottom: 0;
+    padding-bottom: 10px;
+    background-color: white;
+}
+.footer-button-plus {
+    width: 80px;
+    margin: auto;
+    text-align: center;
+    cursor: pointer;
+    font-size: 24px;
+    padding-top: 12px;
+}
+.sample-box {
+    width: 100%;
+    height: 600px;
+    background-color: bisque;
+}
+.inputfile {
+    display: none;
+}
+.input-plus {
+    cursor: pointer;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    box-sizing: border-box;
+    font-family: "consolas";
+    margin-top: 60px;
+    width: 100%;
+    max-width: 460px;
+    margin: auto;
+    position: relative;
+    border-right: 1px solid #eee;
+    border-left: 1px solid #eee;
 }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,86 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div v-if="props.step === 0">
+        <Post :listData="props.listData" />
+    </div>
+
+    <div v-if="props.step === 1">
+        <div class="upload-image" :style="`background-image: url(${props.fileInfo})`"></div>
+        <div class="filters">
+            <div class="filters-1"></div>
+            <div class="filters-1"></div>
+            <div class="filters-1"></div>
+            <div class="filters-1"></div>
+            <div class="filters-1"></div>
+        </div>
+    </div>
+
+    <!-- 글작성페이지 -->
+    <div v-if="props.step === 2">
+        <div class="upload-image" :style="`background-image: url(${props.fileInfo})`"></div>
+        <div class="write">
+            <textarea class="write-box" v-model="contents">write!</textarea>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, ref, defineEmits } from "vue";
+import Post from "./Post.vue";
+const contents = ref("");
+const props = defineProps({
+    listData: {
+        type: Object, // 객체 형식의 prop을 명시
+    },
+    step: null,
+    fileInfo: String,
+});
+
+const emit = defineEmits(["addData"]);
+emit("addData", contents);
+</script>
+
+<style>
+.upload-image {
+    width: 100%;
+    height: 450px;
+    background: cornflowerblue;
+    background-size: cover;
+}
+.filters {
+    overflow-x: scroll;
+    white-space: nowrap;
+}
+.filter-1 {
+    width: 100px;
+    height: 100px;
+    background-color: cornflowerblue;
+    margin: 10px 10px 10px auto;
+    padding: 8px;
+    display: inline-block;
+    color: white;
+    background-size: cover;
+}
+.filters::-webkit-scrollbar {
+    height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+    background: #f1f1f1;
+}
+.filters::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+    background: #555;
+}
+.write-box {
+    border: none;
+    width: 90%;
+    height: 100px;
+    padding: 15px;
+    margin: auto;
+    display: block;
+    outline: none;
+}
+</style>
